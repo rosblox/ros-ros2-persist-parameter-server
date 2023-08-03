e29127387ea4f3da6172998b7c563d017852e05f
Max Polzin <polzin.max@gmail.com>
2023-08-03 21:28:16 +0200

    Mount example file for dev container.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 docker run -it --rm --name=ros-ros2-persist-parameter-server \
 --volume $(pwd)/ros2_persist_parameter_server:/colcon_ws/src/ros2_persist_parameter_server \
---volume ros_ros2_persist_parameter_server:/tmp \
+--volume $(pwd)/ros2_persist_parameter_server/server/param/parameter_server.yaml:/tmp/parameter_server.yaml \
 --ipc=host --pid=host \
 --network=host \
 --env UID=$(id -u) \
